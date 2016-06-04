ab26171124473ed57ecfd3c7daf719f5571d68c6
Denis Baskovsky <qertis@users.noreply.github.com>
2016-06-04 16:17:45 +0300

    Update dropbox-sdk-js.js

--- dropbox-sdk-js.js
+++ dropbox-sdk-js.js
@@ -1,23 +1,42 @@
-'use strict';
-class Dropbox {
+export class Dropbox {
 
+  /**
+   * Конструктор
+   * @param config {Object}
+   */
   constructor(config) {
     this.config = config;
     this._hashTrack();
   }
 
+  /**
+   * Ссылка на хост
+   * @returns {string}
+   */
   get hostAPI() {
     return 'https://api.dropboxapi.com';
   }
 
+  /**
+   * Ссылка на контент
+   * @returns {string}
+   */
   get hostContent() {
     return 'https://content.dropboxapi.com';
   }
 
+  /**
+   * Установка токена
+   * @param _token {string}
+   */
   set token(_token) {
     localStorage.setItem('__db_token', _token);
   }
 
+  /**
+   * Извлечение токена
+   * @returns {*}
+   */
   get token() {
 
     const dbToken = localStorage.getItem('__db_token');
@@ -29,20 +48,76 @@ class Dropbox {
 
   }
 
+  /**
+   * Аутентификация
+   * @return {Promise}
+   * @public
+   */
   authenticate() {
-    localStorage.removeItem('__db_token');
 
-    const clientId = this.config.appKey;
-    const redirectUri = this.config.redirectUri;
+    return new Promise((resolve, reject) => {
 
-    const link = `https://www.dropbox.com/1/oauth2/authorize` +
-      `?client_id=${ encodeURIComponent(clientId) }` +
-      `&redirect_uri=${ encodeURIComponent(redirectUri) }` +
-      `&response_type=token`;
+      const self = this;
+      const clientId = this.config.appKey;
+      const redirectUri = this.config.redirectUri;
+      let authWindow;
 
-    window.open(link, '_blank', 'width=540,height=960,left=0,top=0,menubar=yes,toolbar=no,scrollbars=yes');
+      localStorage.removeItem('__db_token');
+
+      const link = `https://www.dropbox.com/1/oauth2/authorize` +
+        `?client_id=${ encodeURIComponent(clientId) }` +
+        `&redirect_uri=${ encodeURIComponent(redirectUri) }` +
+        `&response_type=token`;
+
+      const windowProperties = 'location=no,toolbar=no';
+
+      // if cordova available use InAppBrowser
+      // else use location change
+      if (window.cordova && typeof window.cordova === 'object' && cordova.InAppBrowser) {
+        authWindow = cordova.InAppBrowser.open(link, '_blank', windowProperties);
+      } else {
+        authWindow = window.open(link, '_blank', windowProperties);
+      }
+
+      authWindow.addEventListener('loadstart', loadstart);
+
+      function loadstart(e) {
+        const url = e.url;
+
+        if (url) {
+          const tokenMatch = /access_token=(.+)&token_type/.exec(e.url);
+
+          if (tokenMatch) {
+            authWindow.removeEventListener('loadstart', loadstart);
+            self._tokenMatch(tokenMatch, authWindow);
+            resolve(self.token);
+          } else {
+            reject();
+          }
+        } else {
+          reject();
+        }
+      }
+
+    });
+
   }
 
+  /**
+   * Установка токена и закрытие окна
+   * @param tokenMatch {Array}
+   * @param _window {Window}
+   * @private
+   */
+  _tokenMatch(tokenMatch, _window) {
+    this.token = tokenMatch[1];
+    _window.close();
+  }
+
+  /**
+   * Отзыв токена
+   * @returns {Promise.<T>}
+   */
   revokeToken() {
 
     return this.request({
@@ -57,6 +132,12 @@ class Dropbox {
 
   }
 
+  /**
+   * Загрузка файлов
+   * @param path
+   * @param rev
+   * @returns {Promise.<T>}
+   */
   filesDownload(path, rev = null) {
 
     return this.request({
@@ -74,13 +155,23 @@ class Dropbox {
 
   }
 
-  filesUpload(file, path, mode, autorename, client_modified, mute) {
+  /**
+   * Выгрузка файлов
+   * @param body
+   * @param path
+   * @param mode
+   * @param autorename
+   * @param client_modified
+   * @param mute
+   * @returns {Promise.<T>}
+   */
+  filesUpload(body, path, mode, autorename, client_modified, mute) {
 
     return this.request({
       host: this.hostContent,
       query: '/2/files/upload',
       method: 'POST',
-      body: file,
+      body,
       type: Blob,
       headers: {
         type: 'application/octet-stream',
@@ -96,37 +187,64 @@ class Dropbox {
 
   }
 
+  /**
+   * Генерация хедеров для запроса
+   * @param _headers {Object}
+   * @returns {Object}
+   * @private
+   */
   _generateHeadersFrom(_headers = {}) {
+    const accept = 'Accept';
+    const authorization = 'Authorization';
+    const contentType = 'Content-Type';
+    const dropboxAPIArg = 'Dropbox-API-Arg';
 
-    const headers = new window.Headers();
-    headers.set('Accept', 'application/json');
-    headers.set('Authorization', 'Bearer ' + this.token);
+    let headers = {};
+    headers[accept] = 'application/json';
+    headers[authorization] = `Bearer ${ this.token }`;
 
     if (_headers.type) {
-      headers.set('Content-Type', _headers.type);
+      headers[contentType] = _headers.type;
     }
 
     if (_headers.arg) {
-      headers.set('Dropbox-API-Arg', JSON.stringify(_headers.arg));
+      headers[dropboxAPIArg] = JSON.stringify(_headers.arg);
     }
 
     return headers;
   }
 
+  /**
+   * Генерация тела запроса
+   * @param body
+   * @returns {*|null}
+   * @private
+   */
   _generateBodyFrom(body) {
     return body || null;
   }
 
-  generateMethodFrom(method) {
+  /**
+   * Генерация метода запроса
+   * @param method {string}
+   * @returns {string}
+   */
+  _generateMethodFrom(method) {
     return method || 'POST';
   }
 
+  /**
+   * Генерация урла
+   * @param params {Object]
+   * @returns {string}
+   * @private
+   */
   _generateRequestURIFrom(params) {
     return `${ params.host }${ params.query }`;
   }
 
   /**
-   * TODO: Сделать проверку на ошибку
+   * Трекинг хэша для извлечения токена
    * @private
    */
   _hashTrack() {
@@ -142,13 +260,8 @@ class Dropbox {
       const tokenMatch = params[0].match(/token=(.+)$/);
 
       if (tokenMatch) {
-        // var tokenType = params[1].match(/type=(.+)/);
-        // var uid = params[2].match(/uid=(.+)/);
-        // localStorage.setItem('__db_token_type', tokenType[1]);
-        // localStorage.setItem('__db_uid', uid[1]);
-
-        self.token = tokenMatch[1];
-        window.close();
+        window.removeEventListener('hashchange', onhashchange);
+        self._tokenMatch(tokenMatch, window);
       }
 
     }
@@ -156,13 +269,13 @@ class Dropbox {
   }
 
   /**
-   *
+   * Запрос
    * @param params {Object}
    * @returns {Promise.<T>}
    */
   request(params = {}) {
     const requestURI = this._generateRequestURIFrom(params);
-    const method = this.generateMethodFrom(params.method);
+    const method = this._generateMethodFrom(params.method);
     const body = this._generateBodyFrom(params.body);
     const headers = this._generateHeadersFrom(params.headers);
 
